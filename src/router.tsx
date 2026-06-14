69439189a3172d3c8892283ac7e688d36b50c398
Caio Camacho <cmnc212@gmail.com>
2026-06-14 08:38:42 -0400

    fix: use hash routing for GitHub Pages interactions

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,14 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
+import { createHashHistory, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
-  const basepath = import.meta.env.BASE_URL === "/" ? undefined : import.meta.env.BASE_URL.replace(/\/$/, "");
 
   const router = createRouter({
     routeTree,
+    history: createHashHistory(),
     context: { queryClient },
-    basepath,
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
